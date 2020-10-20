4a00330ee15c06aadd84b2b53682344ed9e53da9
Aizeke <iguido97@icloud.com>
2020-10-19 21:53:10 -0700

    First Herkou Deployment

--- server/server.js
+++ server/server.js
@@ -9,5 +9,5 @@ app.use(express.json())
 app.use('/api/nfl', apiRouter)
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running on port: 3000')
+  console.log(`Server is running on port: ${process.env.PORT || 3000}`)
 })
