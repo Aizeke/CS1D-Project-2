1f3dce243ebec20aa48b2d435df5ed2098db6f5b
Daniel Navarro <60456378+Sakezone@users.noreply.github.com>
2020-11-20 18:48:54 -0800

    fix login page

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, './assets/pages/login.html'))
+})
 app.get('/admin', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/adminpage.html'))
 })
